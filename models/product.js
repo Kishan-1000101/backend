8313182650f58a522fabff26168aa6d1c4b55eb4
Kishan-1000101 <wisdomkishan@gmail.com>
2024-02-13 22:13:37 +0400

    commit

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  body_temperature: {
+    type: Number,
+  },
+  pulse_rate: {
+    type: Number,
+  },
+  bmi: {
+    type: Number,
+  },
+  human_presence: {
+    type: Boolean,
+  },
+  medicine_reminder: {
+    hour: {
+      type: Number,
+    },
+    minute: {
+      type: Number,
+    },
+    second: {
+      type: Number,
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
